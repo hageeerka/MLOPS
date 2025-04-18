87017e27f8a9da06f5351e0c93fc54a62d999684
hageeerka <150540481+hageeerka@users.noreply.github.com>
2025-04-19 00:36:43 +0500

    Update train_model.py

--- mlops/jenkins/train_model.py
+++ mlops/jenkins/train_model.py
@@ -1,7 +1,5 @@
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
-import seaborn as sns
 
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import SGDRegressor,LinearRegression, Lasso, Ridge
